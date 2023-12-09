6c70ec7447a83b9ee684b1da99824787f92ee449
Evgeny <knyazev.evgeny@gmail.com>
2023-12-09 18:14:11 +0400

    resolved

--- app/src/main/java/com/example/a001/MainActivity.kt
+++ app/src/main/java/com/example/a001/MainActivity.kt
@@ -2,40 +2,28 @@ package com.example.a001
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-private  val counterString = "counter"
-
 class MainActivity : AppCompatActivity() {
-    private lateinit var button: Button
+    private lateinit var editView: EditText
     private lateinit var textView: TextView
-    private var counter: Int = 0
+    private lateinit var button: Button
+    private var counter: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.countTextView)
+        editView = findViewById(R.id.editTextText)
         button = findViewById(R.id.button)
         button.setOnClickListener {
             count()
         }
     }
 
-    // Сохранение в стэйт
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(counterString, counter)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        counter = savedInstanceState.getInt(counterString)
-        textView.text = counter.toString()
-    }
-
     private fun count () {
-        counter ++
-        textView.text = counter.toString()
+        textView.text = editView.text
     }
 }
