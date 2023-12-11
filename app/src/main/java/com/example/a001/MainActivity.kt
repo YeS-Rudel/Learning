6b2f7882ae98bfefe664ad84659cc21e06808f75
Evgeny <knyazev.evgeny@gmail.com>
2023-12-11 11:25:42 +0400

    Добавил маску

--- app/src/main/java/com/example/a001/MainActivity.kt
+++ app/src/main/java/com/example/a001/MainActivity.kt
@@ -10,12 +10,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import android.net.Uri
 
+private  const val mask: String = "+9(999)99-99-999"
+private const val NUMBER_MASK = '9'
 
 class MainActivity : AppCompatActivity() {
     private lateinit var editView: EditText
     private lateinit var textView: TextView
     private lateinit var button: Button
-//    private var counter: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +28,7 @@ class MainActivity : AppCompatActivity() {
             count()
         }
         editView.addTextChangedListener { text ->
-            textView.text = text
+            textView.text = check(text.toString())
         }
     }
 
@@ -35,5 +36,24 @@ class MainActivity : AppCompatActivity() {
         textView.text = editView.text
         val intent = Intent(ACTION_DIAL, Uri.parse("tel:"+editView.text))
         startActivity(intent)
+    }
+
+    private fun check(text: String) : String {
+        var value = ""
+        var length = text.length
+        var index = 0
+        var textCount = 0
+        while (length > 0) {
+            if (mask[index] == NUMBER_MASK) {
+                value += text[textCount]
+                textCount ++
+                index ++
+                length --
+            } else {
+                value += mask[index]
+                index ++
+            }
+        }
+        return value
     }
 }
