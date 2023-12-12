bbedd643cac676428be688f415cee4ead3698145
Evgeny <knyazev.evgeny@gmail.com>
2023-12-12 12:17:21 +0400

    Watcher

--- app/src/main/java/com/example/a001/MainActivity.kt
+++ app/src/main/java/com/example/a001/MainActivity.kt
@@ -5,72 +5,33 @@ import android.content.Intent.*
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.addTextChangedListener
 import android.net.Uri
-
-private  const val mask: String = "+9 (999) 99-99-999"
-private const val NUMBER_MASK = '9'
 
 class MainActivity : AppCompatActivity() {
     private lateinit var editView: EditText
-    private lateinit var textView: TextView
     private lateinit var button: Button
+    private lateinit var watcher: MaskWorker
+    private var text = ""
 
-    private var current = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.countTextView)
         editView = findViewById(R.id.editTextText)
+        watcher = MaskWorker(editView) {
+            text = it
+        }
         button = findViewById(R.id.button)
         button.setOnClickListener {
             call()
         }
-        editView.addTextChangedListener { text ->
-            if (text.toString() != current) {
-                if (text.toString().length  <= mask.length ) {
-                    check(getValue(text.toString()))
-                    textView.text = editView.text
-                } else {
-                    editView.setText(text.toString().substring(0, text.toString().length - 1))
-                    editView.setSelection(editView.length())
-                }
-            }
-        }
+        editView.addTextChangedListener(watcher)
     }
 
-    private fun call () {
-        val intent = Intent(ACTION_DIAL, Uri.parse("tel:"+textView.text))
+    private fun call() {
+        val intent = Intent(ACTION_DIAL, Uri.parse("tel:"+text))
         startActivity(intent)
-    }
-
-    private fun getValue(text: String) : String {
-        return  text.filter { char ->
-            char.isDigit()
-        }
-    }
-
-    private fun check(text: String) {
-        var value = ""
-        var length = text.length
-        var index = 0
-        var textCount = 0
-        while (length > 0) {
-            if (mask[index] == NUMBER_MASK) {
-                value += text[textCount]
-                textCount ++
-                index ++
-                length --
-            } else {
-                value += mask[index]
-                index ++
-            }
-        }
-        current = value
-        editView.setText(value)
-        editView.setSelection(editView.length())
     }
 }
---
+++ app/src/main/java/com/example/a001/MaskWorker.kt
@@ -0,0 +1,59 @@
+package com.example.a001
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
+private const val mask: String = "+9 (999) 99-99-999"
+private const val NUMBER_MASK = '9'
+
+class MaskWorker(view: EditText, handler: (String) -> Unit) : TextWatcher {
+
+    private var current = ""
+    private val view: EditText = view
+    private var handler: (String) -> Unit = handler
+
+    private fun getValue(text: String): String {
+        return text.filter { char ->
+            char.isDigit()
+        }
+    }
+
+    private fun check(text: String) {
+        var value = ""
+        var length = text.length
+        var index = 0
+        var textCount = 0
+        while (length > 0) {
+            if (mask[index] == NUMBER_MASK) {
+                value += text[textCount]
+                textCount++
+                index++
+                length--
+            } else {
+                value += mask[index]
+                index++
+            }
+        }
+        current = value
+        handler(getValue(current))
+        view.setText(value)
+        view.setSelection(value.length)
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+    override fun afterTextChanged(s: Editable?) {
+        val text = s.toString()
+        if (text != current) {
+            if (text.length <= mask.length) {
+                check(getValue(text))
+            } else {
+                view.setText(text.substring(0, text.length - 1))
+                view.setSelection(view.text.length)
+            }
+        }
+    }
+}
