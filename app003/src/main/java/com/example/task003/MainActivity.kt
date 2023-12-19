a681513c391461a8804e92f877d9fe4a6f4e79d2
Evgeny <knyazev.evgeny@gmail.com>
2023-12-19 16:40:36 +0400

    Возврат на экран с текстом

--- app003/src/main/java/com/example/task003/MainActivity.kt
+++ app003/src/main/java/com/example/task003/MainActivity.kt
@@ -5,26 +5,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val MAIN_TEXT = "text"
+    }
+
     private lateinit var editView: EditText
-    private lateinit var button: Button
-    private var text = ""
+
+    var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            when (it.resultCode) {
+                SecondScreenActivity.RESULT_CODE -> {
+                    val text = it.data?.extras?.getString(MainActivity.MAIN_TEXT, "").orEmpty()
+                    editView.setText(text)
+                }
+            }
+
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        editView = findViewById(R.id.editTextText)
-        button = findViewById(R.id.button)
+        editView = findViewById<EditText>(R.id.editTextText)
+        val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
             save(string = editView.text.toString())
         }
     }
 
     private fun save(string: String) {
-        this.text = string
         val intent = Intent(this, SecondScreenActivity::class.java)
-        intent.putExtra(SecondScreenActivity.TEXT, text)
-        startActivity(intent)
+        intent.putExtra(SecondScreenActivity.SECOND_TEXT, string)
+        resultLauncher.launch(intent)
     }
 }
--- app003/src/main/java/com/example/task003/SecondScreenActivity.kt
+++ app003/src/main/java/com/example/task003/SecondScreenActivity.kt
@@ -1,25 +1,37 @@
 package com.example.task003
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class SecondScreenActivity : AppCompatActivity() {
 
     companion object {
-        const val TEXT = "text"
+        const val SECOND_TEXT = "text"
+        const val RESULT_CODE = 1001
     }
 
-    var text = ""
+    private var text = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_screen)
-        text = intent.extras?.getString(TEXT, "").orEmpty()
-        this.findViewById<TextView>(R.id.secondTextView).text = text
+        text = intent.extras?.getString(SECOND_TEXT, "").orEmpty()
+        val editView = this.findViewById<TextView>(R.id.secondTextView)
+        editView.text = text
         setSupportActionBar(this.findViewById(R.id.toolbar))
         supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
+        }
+        val button = findViewById<Button>(R.id.secondButton)
+        button.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra(MainActivity.MAIN_TEXT, editView.text.toString())
+            setResult(RESULT_CODE, intent)
+            finish()
         }
     }
 }
