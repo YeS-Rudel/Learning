98508ebab20be69546f61799625cb85ac25807d8
Evgeny <knyazev.evgeny@gmail.com>
2023-12-20 16:44:07 +0400

    listView

--- app004/src/main/java/com/example/app004/MainActivity.kt
+++ app004/src/main/java/com/example/app004/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.app004
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var items = arrayOf(
+        "Молоко", "Сметана", "Колбаска", "Сыр", "Мышка",
+        "Ананас", "Икра черная", "Икра кабачковая", "Яйцо"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val list = findViewById<ListView>(R.id.listView)
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_multiple_choice, items
+        )
+        list.adapter = adapter
     }
 }
